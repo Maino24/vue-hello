8a3825cc842926e8b87dcdeab5458505cd2a8219
Maino24 <mg02.gabriele@gmail.com>
2022-11-10 18:13:51 +0100

    inserita frase

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 var root = new Vue({
 
 el: `#root`,
+
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
 data:{
-    message: `Hello World`
+    message: ``
+    Image: `https://picsum.photos/200` 
+},
+
+methods: {
+    click: function (){
+
+       return  this.message = `text-primary`
+
+    }
 }
 
 })
