bf1e2885805ce54db93ac7679f78d89f06edab4c
Maino24 <mg02.gabriele@gmail.com>
2022-11-10 18:22:52 +0100

    frase

--- main.js
+++ main.js
@@ -5,16 +5,20 @@ el: `#root`,
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 
 data:{
-    message: ``
-    Image: `https://picsum.photos/200` 
+    message: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam omnis porro asperiores inventore assumenda, aperiam, non, ullam obcaecati quo ex facere magni deleniti nostrum eos expedita et harum earum. Atque.`
 },
 
+/*
 methods: {
     click: function (){
 
        return  this.message = `text-primary`
 
     }
-}
+},
+*/
+//template: '<div>{{ message }}</div>'
 
 })
+
+//root.message = 'Hello!'
